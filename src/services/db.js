800e38549c055fe8a6cbbc67acb3a33520e48d49
Cláudio de Oliveira Pereira <claudio.pereira@inpa.gov.br>
2021-10-03 17:19:05 -0400

    feat: add service productsDB

---
+++ src/services/db.js
@@ -0,0 +1,8 @@
+export const productsDB = [
+  { id: 1, name: "Smart TV LED 50", price: 1999.0 },
+  { id: 2, name: "PlayStation 5", price: 12000.0 },
+  { id: 3, name: "Notebook Acer Nitro 5", price: 5109.72 },
+  { id: 4, name: "Headset s fio Logitech G935", price: 1359.0 },
+  { id: 5, name: "Tablet Samsung Galaxy Tab S7", price: 4844.05 },
+  { id: 6, name: "Cadeira Gamer Cruiser Preta FORTREK", price: 1215.16 },
+];
